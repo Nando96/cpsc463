74ec42e7b4cfae24088c145960119f8f50551d58
hellochloe92 <45779918+hellochloe92@users.noreply.github.com>
2019-04-05 03:03:47 -0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/nav.css" type="text/css"/>
+    <title>Food Online Ordering System</title>
+  </head>
+  <body>
+    <div class="sidenav">
+      <a href="main.php">Home</a>
+      <a href="login.php">Login</a>
+      <a href="signupForm.php">Register</a>
+      <a href="#menu">Menu</a>
+      <a href="#contact">Contact</a>
+    </div>
+<!-- Page content -->
+    <div class="main">
+      <h2>Welcome</h2>
+      <p>Food Online Ordering System</p>
+    </div>
+    <footer>
+  			<p2>copyright &copy; 2019 by the Company<br/></p2>
+  	</footer>
+  </body>
+</html>
